a30a8b27ca7c49bf5b039e91fc1a8a9c6f51eae5
Sonja Parsell <hello@sparsell.com>
2020-03-16 16:53:41 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -53,12 +53,12 @@ def over?(board)
 end
 
 #return the token, "X" or "O" that has won the game given a winning board.
-def winner?(board)
+def winner(board)
   WIN_COMBINATIONS.detect do |win_combo|
     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
-      return "X won!"
+      return "X"
     elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
-      return "O won!"
+      return "O"
     else
       nil
   end
